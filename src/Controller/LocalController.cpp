62a31c414b4812b7def3d24ec1236b8f779d1171
Mariana Fonseca <marifonsecareis001@gmail.com>
2025-06-24 13:08:17 -0300

    Criação da classe local e metodo criar local
    Foi feita uma validação em criar local, no qual o mesmo só será possível se o endereço digitado conter alguma letra.

--- src/Controller/LocalController.cpp
+++ src/Controller/LocalController.cpp
@@ -4,6 +4,7 @@
 #include "LocalController.h"
 
 #include <iostream>
+#include <cstring>
 
 #include "../Utils/EnumMenu.h"
 #include "../Utils/ExibirMensagem.h"
@@ -11,6 +12,32 @@
 LocalController::LocalController(LocalService &localService): localService(localService) {
 }
 
+class local{
+private:
+    float cord_x;
+    float cord_y;
+    char endereco[100];
+
+public:
+    local(float x, float y, const char end[]) {
+        cord_x = x;
+        cord_y = y;
+        strcpy(endereco, end);
+    }
+
+    void setX(float x) { cord_x = x; }
+    void setY(float y) { cord_y = y; }
+
+
+    float getX() const { return cord_x; }
+    float getY() const { return cord_y; }
+    const char* getEndereco() const { return endereco; }
+
+    void mostrar() const {
+        std::cout << "Endereço: " << endereco
+                  << "\nCoordenadas: (" << cord_x << ", " << cord_y << ")\n";
+    }
+};
 
 void LocalController::menu() {
     char teclaGlobal = '\0';
@@ -36,6 +63,7 @@ void LocalController::menu() {
 
         switch (teclaGlobal) {
             case CRIAR_LOCAL:
+                criar();
                 break;
             case EXCLUIR_LOCAL:
                 break;
@@ -50,10 +78,43 @@ void LocalController::menu() {
     }
 }
 
+bool validarEndereco(const char end[]) {
+    bool temLetra = false;
+    for (int i = 0; end[i] != '\0'; i++) {
+        char c = end[i];
+        if (isalpha(c)) {
+            temLetra = true;
+        } else if (isdigit(c) || c == ' ') {
+            continue;
+        } else {
+            return false;
+        }
+    }
+    return temLetra;
+}
 
 void LocalController::criar() {
+    float x, y;
+    char endereco[100];
 
+    std::cout << "Digite a coordenada X: ";
+    std::cin >> x;
+
+    std::cout << "Digite a coordenada Y: ";
+    std::cin >> y;
+
+    std::cin.ignore();
+    std::cout << "Digite o endereço (Ex: 'Sao Paulo', 'Avenida X, 123'): ";
+    std::cin.getline(endereco, 100);
+
+    if (!validarEndereco(endereco)) {
+        std::cout << "Endereço inválido. O local não foi criado.\n";
+        return;
+    }
+
+    std::cout << "Local criado com sucesso!\n";
 }
+
 
 void LocalController::excluir() {
 
