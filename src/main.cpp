f0d63a8755f449ff466ac7464cf2ef779d2e1fe8
Oieusouopi <rafasc866@gmail.com>
2025-06-26 13:09:13 -0300

    rebasing

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 
 #include "App.h"
-#include "Controller/VeiculoController.h"
-#include "Service/VeiculoService.h"
 #include <windows.h>
+
 
 // void menuBanco(); // Tenho que pensar onde vou colocar esse menu
 
@@ -16,4 +15,40 @@ int main() {
     app.executar();
     return 0;
 }
+
+// void menuBanco() {
+//     char teclaGlobal = '\0';
+//     std::string entrada;
+//
+//     while (true) {
+//
+//         std::cout << "\n----- MENU BANCO -----\n";
+//         std::cout << static_cast<char>(SALVAR_TODOS_DADOS) << " -  Salvar todos dados\n";
+//         std::cout << static_cast<char>(RESTAURAR_DADOS_DE_BACKUPS_ANTIGOS) << " -  Restaurar dados de backups antigos\n";
+//         std::cout << static_cast<char>(VOLTAR_PARA_O_MENU_PRINCIPAL_BANCO) << " -  Voltar para o menu principal\n";
+//         std::cout << "-----------------------------\n";
+//
+//         std::cin >> entrada;
+//
+//         if (entrada.length() != 1) {
+//             ExibirMensagem::exibirMensagemDeErro();
+//             continue;
+//         }
+//
+//         teclaGlobal = entrada[0];
+//
+//         switch (teclaGlobal) {
+//             case SALVAR_TODOS_DADOS:
+//                 break;
+//             case RESTAURAR_DADOS_DE_BACKUPS_ANTIGOS:
+//                 break;
+//             case VOLTAR_PARA_O_MENU_PRINCIPAL_BANCO:
+//                 return;
+//             default:
+//                 ExibirMensagem::exibirMensagemDeErro();
+//                 break;
+//         }
+//
+//     }
+// }
 
