5f0bd7fa7e279e65ff8cba605caa76aa4cf9ac22
Oieusouopi <rafasc866@gmail.com>
2025-06-26 12:58:44 -0300

    Class Local.h temporaria

--- src/Model/Local.h
+++ src/Model/Local.h
@@ -4,16 +4,11 @@
 
 #ifndef LOCAL_H
 #define LOCAL_H
-#include <string.h>
 
 class Local {
 public:
-    Local(char *nome) {
-        strncpy(this->nome, nome, sizeof(this->nome - 1));
-        this->nome[sizeof(this->nome) - 1] = '\0';
-
-    }
-    char nome[30];
+    Local(std::string nome): nome(nome) {}
+    std::string nome;
 
 };
 
